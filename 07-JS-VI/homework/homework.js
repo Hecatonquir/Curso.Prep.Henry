280846661105a0678807f8c04e53d43dd9c280e9
Hecatonquir <maxicuentas92@hotmail.com>
2022-02-16 01:06:43 -0300

    Html Homework done

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -3,75 +3,74 @@
 const { default: simpleGit } = require("simple-git");
 
 function mayuscula(nombre) {
-  //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
-  //ej: Recibe "mario" ----> Devuelve "Mario"
-  //Tu código:
+	//La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
+	//ej: Recibe "mario" ----> Devuelve "Mario"
+	//Tu código:
 
-  var string = nombre.split("");
-  string [0] = string[0].toUpperCase();
-  return string.join("");  
+	var string = nombre.split("");
+	string[0] = string[0].toUpperCase();
+	return string.join("");
 
-  // OTRA FORMA ES: return nombre.charAt(0).toUpperCase() + nombre.slice(1);
+	// OTRA FORMA ES: return nombre.charAt(0).toUpperCase() + nombre.slice(1);
 }
 
-
 function invocarCallback(cb) {
-  // Invoca al callback `cb`
-  //Tu código:
+	// Invoca al callback `cb`
+	//Tu código:
 
-  return cb();
+	return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
-  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
-  //Devolver el callback pasándole como argumentos los números recibidos.
-  //Tu código:
+	//Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
+	//Devolver el callback pasándole como argumentos los números recibidos.
+	//Tu código:
 
-  return cb(n1, n2);
+	return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
-  // Suma todos los números enteros (int/integers) de un array ("numeros")
-  // Pasa el resultado a `cb`
-  // No es necesario devolver nada
-  //Tu código:
-  //var sumatoria = numeros.reduce(cb(0));
-  //return acumulador + elemento
+	// Suma todos los números enteros (int/integers) de un array ("numeros")
+	// Pasa el resultado a `cb`
+	// No es necesario devolver nada
+	//Tu código:
+	//var sumatoria = numeros.reduce(cb(0));
+	//return acumulador + elemento
 
-  var suma = numeros.reduce(function(accumulador,current){
-    return accumulador + current; 
-  });
-  cb(suma);
+	var suma = numeros.reduce(function (accumulador, current) {
+		return accumulador + current;
+	});
+	cb(suma);
 }
 
 function forEach(array, cb) {
-  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
-  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
-  //Tu código:
+	// Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+	// Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+	//Tu código:
 
-  array.forEach(function(elemento) {
-    cb(elemento);
-  })
+	array.forEach(function (elemento) {
+		cb(elemento);
+	});
 }
 
 function map(array, cb) {
-  // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
-  // El nuevo array debe tener la misma longitud que el array del argumento
-  //Tu código:
+	// Crea un nuevo array
+	// Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+	// El nuevo array debe tener la misma longitud que el array del argumento
+	//Tu código:
 
-  const vector = array.map(function(elemento) {
-   return cb(elemento);
-  });
-  return vector;
+	const vector = array.map(function (elemento) {
+		return cb(elemento);
+	});
+	return vector;
 }
 
 function filter(array) {
-  //Filtrar todos los elementos del array que comiencen con la letra "a".
-  //Devolver un nuevo array con los elementos que cumplen la condición
-  //Tu código:
+	//Filtrar todos los elementos del array que comiencen con la letra "a".
+	//Devolver un nuevo array con los elementos que cumplen la condición
+	//Tu código:
 
-  /*var filtrado = [];
+	/*var filtrado = [];
   const filtro = array.map(function(elemento) {
     if (elemento[0] === "a"){
       filtrado.push(elemento);
@@ -79,18 +78,18 @@ function filter(array) {
   })  
   return (filtrado);*/
 
-  return array.filter(palabras => palabras[0] === "a") 
+	return array.filter((palabras) => palabras[0] === "a");
 }
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
-  mayuscula,
-  invocarCallback,
-  operacionMatematica,
-  sumarArray,
-  forEach,
-  map,
-  filter
+	mayuscula,
+	invocarCallback,
+	operacionMatematica,
+	sumarArray,
+	forEach,
+	map,
+	filter,
 };
